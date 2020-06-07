f7232c7e55f09704bbae51ea47f3b52f76e9a579
khadija <notaclichegirl@gmail.com>
2020-06-08 00:27:45 +0400

    Added server, clients and client test files

---
+++ Task 1/client_test.py
@@ -0,0 +1,51 @@
+import unittest
+from client3 import getDataPoint, getRatio
+
+class ClientTest(unittest.TestCase):
+  def test_getDataPoint_calculatePrice(self):
+    quotes = [
+      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+       self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price']) / 2))
+
+  def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
+    quotes = [
+      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+       self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price']) / 2))
+
+  """ ------------ Add more unit tests ------------ """
+  def test_getRatio_priceBIsZeroReturnsNone(self):
+    price_a = 121.2
+    price_b = 0
+    self.assertIsNone(getRatio(price_a, price_b))
+
+  def test_getRatio_priceAisZeroReturnsZero(self):
+    price_a = 0
+    price_b = 121.
+    self.assertEqual(getRatio(price_a, price_b), 0)
+  
+  def test_getRatio_priceAIsGreater_returnsGreaterThan1(self):
+    price_a = 250.50
+    price_b = 125.50
+    self.assertGreater(getRatio(price_a, price_b), 1)
+  
+  def test_getRatio_priceBIsGreater_returnsLessThan1(self):
+    price_a = 125.50
+    price_b = 250.50
+    self.assertLess(getRatio(price_a, price_b), 1)
+  
+  def test_getRatio_priceAPriceB_equal_returns1(self):
+    price_a = 125.50
+    price_b = 125.50
+    self.assertEqual(getRatio(price_a, price_b), 1)
+    
+
+if __name__ == '__main__':
+    unittest.main()
